60a8e4bf9ce708592a77954ccc573c700166b2e4
Samson Thomas Juma <124863773+samsonthomas951@users.noreply.github.com>
2025-08-30 15:20:08 +0300

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,5 @@
 // const API_BASE_URL = window.location.origin;
-const API_BASE_URL =  'https://samson69.pythonanywhere.com/api/v1'
+const API_BASE_URL =  'https://b6f3243efdb7.ngrok-free.app/api/v1'
 // const API_BASE_URL = 'http://localhost:5000/api/v1'
 
 // Store form data - now includes dynamic categories
